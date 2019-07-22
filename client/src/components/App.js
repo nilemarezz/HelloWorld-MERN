6327999ab1c1b9afb1bcae39f7d4b81bce3f5b83
Matas <nilenon@gmail.com>
2019-07-22 12:53:55 +0700

    [Add] react-strap etc

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import  'bootstrap/dist/css/bootstrap/bootstrap.min.css'
+const App = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+export default App ;
