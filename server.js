af142b6f050107fd9d5c882b8c5167e3eb4085a8
Matas <nilenon@gmail.com>
2019-07-22 11:32:20 +0700

    [Edit] Config DB

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-require('dotenv').config()
-const experss = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+require('dotenv').config();
+const experss = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = experss();
 
 // BodyParser MiddleWare
 app.use(bodyParser.json());
 // DB Connect
-mongoose.connect(process.env.DB_ADDRESS,{ useNewUrlParser: true });
+mongoose
+  .connect(
+    process.env.DB_ADDRESS,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("DB Connect..."))
+  .catch(err => console.log(err));
 // Listen Port
-app.listen(process.env.PORT || 5000,()=>console.log('Server has Start'))
+app.listen(process.env.PORT || 5000, () => console.log("Server has Start"));
